2e70c45d6639c165878d8e5bd30deda1a59da463
vrl <vrlinux@pm.me>
2021-07-29 18:59:12 +0200

    hotfix

--- web-gui/service.sh
+++ web-gui/service.sh
@@ -3,7 +3,7 @@
 CURRENT_DIR="$(pwd)"
 echo "Building Docker images - this will take a while, please be patient..."
 echo ""
-cd $CURRENT_DIR/docker-pyinstaller1 \
+cd $CURRENT_DIR/web-gui/docker-pyinstaller \
 ; docker build -f Dockerfile-py3-amd64 -t nix-amd64 . \
 ; docker build -f Dockerfile-py3-i386 -t nix-i386 .  \
 ; docker build -f Dockerfile-py3-win32 -t win-x32 .
