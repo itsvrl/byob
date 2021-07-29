410dc478c0fa3a74e2f63e14f90d9dcf767cc064
vrl <vrlinux@pm.me>
2021-07-29 17:34:34 +0200

    Adjusted web-gui/service.sh

--- web-gui/service.sh
+++ web-gui/service.sh
@@ -1,12 +1,13 @@
 #/bin/sh
 # Build Docker images
+CURRENT_DIR="$(pwd)"
 echo "Building Docker images - this will take a while, please be patient..."
 echo ""
-cd $HOME/byob/web-gui/docker-pyinstaller1 \
+cd $CURRENT_DIR/docker-pyinstaller1 \
 ; docker build -f Dockerfile-py3-amd64 -t nix-amd64 . \
 ; docker build -f Dockerfile-py3-i386 -t nix-i386 .  \
 ; docker build -f Dockerfile-py3-win32 -t win-x32 .
 # Run app
-cd $HOME/byob/web-gui/
+cd $CURRENT_DIR
 echo "BYOB WebGUI should now be available on: $HOSTNAME.local:5000"
 python3 run.py
